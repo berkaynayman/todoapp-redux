8e6eda0839cbb112bb5d8c77d532c0113f94e7ff
berkaynayman <berkaynayman4@gmail.com>
2021-12-24 18:27:20 +0300

    new add todo, nanoid generator

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import { useState } from "react"
+import {useDispatch} from "react-redux"
+import {nanoid} from "@reduxjs/toolkit"
+import {addTodo} from "../redux/todos/todosSlice"
 
 function Form() {
+    const [title, setTitle] = useState("")
+    const dispatch = useDispatch()
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        dispatch(addTodo({
+            id: nanoid(5),
+            title,
+            completed: false
+        }))
+        setTitle("")
+    }
+
+
     return (
-        <form>
-            <input className="new-todo" placeholder="What needs to be done?" autoFocus />
+        <form onSubmit={handleSubmit}>
+            <input value={title} onChange={(e) => setTitle(e.target.value)} className="new-todo" placeholder="What needs to be done?" autoFocus />
         </form>
     )
 }
