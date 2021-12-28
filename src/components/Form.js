09cbcee2432e8a890dba1313a0c3a89e2f9662d5
berkaynayman <berkaynayman4@gmail.com>
2021-12-28 15:11:43 +0300

    prepare

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,5 @@
 import { useState } from "react"
 import {useDispatch} from "react-redux"
-import {nanoid} from "@reduxjs/toolkit"
 import {addTodo} from "../redux/todos/todosSlice"
 
 function Form() {
@@ -10,11 +9,7 @@ function Form() {
     const handleSubmit = (e) => {
         e.preventDefault()
         if(!title) return
-        dispatch(addTodo({
-            id: nanoid(5),
-            title,
-            completed: false
-        }))
+        dispatch(addTodo({title}))
         setTitle("")
     }
 
